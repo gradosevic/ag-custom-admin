bd08ab21811d2979f62467128bf07e480f0e4f92
Goran Radosevic <goran.radosevic@outlook.com>
2016-12-05 12:24:23 +0000

    trigger 5.7.1

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: AG Custom Admin
 Plugin URI: http://wordpressadminpanel.com/ag-custom-admin/
 Description: All-in-one tool for admin panel customization. Change almost everything: admin menu, dashboard, login page, admin bar etc. Apply admin panel themes.
 Author: WAP
-Version: 5.7
+Version: 5.7.1
 Text Domain: ag-custom-admin
 Domain Path: /languages
 Author URI: http://www.wordpressadminpanel.com/
@@ -73,7 +73,7 @@ class AGCA{
         /*Initialize properties*/
         $this->colorizer = $this->jsonMenuArray(get_option('ag_colorizer_json'),'colorizer');
 
-        $this->agca_version = "5.7";
+        $this->agca_version = "5.7.1";
 
         //TODO:upload images programmatically
 	}
