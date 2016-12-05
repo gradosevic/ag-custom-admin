25efdcde91824134827a015b6f78b3a70c52711e
Goran Radosevic <goran.radosevic@outlook.com>
2016-12-05 11:09:53 +0000

    Added General Settings tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,3 +2,7 @@
 define('AGCA_SITE_URL','http://wp47.dev');
 define('AGCA_SITE_USERNAME','admin');
 define('AGCA_SITE_PASSWORD','admin');
+define('AGCA_SITE_SUBSCRIBER_USERNAME','subscriber');
+define('AGCA_SITE_SUBSCRIBER_PASSWORD','subscriber');
+define('AGCA_SITE_EDITOR_USERNAME','editor');
+define('AGCA_SITE_EDITOR_PASSWORD','editor');
--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -15,19 +15,20 @@
  * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-    public function login()
+    private function login($username = AGCA_SITE_USERNAME, $password = AGCA_SITE_PASSWORD)
     {
         $I = $this;
         $I->amOnPage('/wp-login.php');
         $I->see('Username or Email Address');
+
         $I->submitForm('#loginform', [
-            'log' => AGCA_SITE_USERNAME,
-            'pwd' => AGCA_SITE_PASSWORD
+            'log' => $username,
+            'pwd' => $password
         ]);
 
         //TODO: Make it work/
@@ -39,29 +40,70 @@ class AcceptanceTester extends \Codeception\Actor
         $I->see('Dashboard');
     }
 
-    public function checkAgcaOption($name){
-        try{
-            $this->click("#agca_form input.agca-checkbox[name=$name]:not(:checked) + div");
-        }catch (Exception $e){}
+    public function loginAsAdmin()
+    {
+        $this->login();
+        return $this;
     }
 
-    public function isAgcaOptionChecked($name){
-        return (bool) $this->executeJS(
-            'return jQuery("#agca_form input.agca-checkbox[name='.$name.']:checked").size()'
+    public function loginAsSubscriber()
+    {
+        $this->login(
+            AGCA_SITE_SUBSCRIBER_USERNAME,
+            AGCA_SITE_SUBSCRIBER_PASSWORD
+        );
+        return $this;
+    }
+
+    public function loginAsEditor()
+    {
+        $this->login(
+            AGCA_SITE_EDITOR_USERNAME,
+            AGCA_SITE_EDITOR_PASSWORD
+        );
+        return $this;
+    }
+
+    public function logOut(){
+        $logOutLink = $this->executeJS('return jQuery("#wp-admin-bar-logout a").attr("href");');
+        $this->amOnUrl($logOutLink);
+        return $this;
+    }
+
+    public function checkAgcaOption($name)
+    {
+        try {
+            if(!$this->isAgcaOptionChecked($name)){
+                $this->click("#agca_form input.agca-checkbox[name=$name]:not(:checked) + div");
+            }
+        } catch (Exception $e) {
+        }
+    }
+
+    public function isAgcaOptionChecked($name)
+    {
+        return (bool)$this->executeJS(
+            'return jQuery("#agca_form input.agca-checkbox[name=' . $name . ']:checked").size()'
         );
     }
 
-    public function uncheckAgcaOption($name){
-        try{
-            $this->click("#agca_form input.agca-checkbox[name=$name]:checked + div");
-        }catch (Exception $e){}
+    public function uncheckAgcaOption($name)
+    {
+        try {
+            if($this->isAgcaOptionChecked($name)){
+                $this->click("#agca_form input.agca-checkbox[name=$name]:checked + div");
+            }
+        } catch (Exception $e) {
+        }
     }
 
-    public function getAGCAOptionLabel($name){
+    public function getAGCAOptionLabel($name)
+    {
         return $this->executeJS("return jQuery(\"label[for=$name]\").text();");
     }
 
-    public function changeAgcaSelectOption($selector, $value){
+    public function changeAgcaSelectOption($selector, $value)
+    {
         $this->executeJS("jQuery(\"#agca_form $selector\").val('$value');");
     }
 }
