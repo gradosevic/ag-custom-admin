2ec51f65a1f5af5a2d779cd2077601dddf534608
Goran Radosevic <gogyinsomnia@gmail.com>
2021-07-12 12:15:51 +0200

    code issues fixed

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-define('AGCA_SITE_URL','http://wp47.dev');
+define('AGCA_SITE_URL','https://wp47.dev');
 define('AGCA_SITE_USERNAME','admin');
 define('AGCA_SITE_PASSWORD','admin');
 define('AGCA_SITE_SUBSCRIBER_USERNAME','subscriber');
